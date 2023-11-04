a81115c475423b83e362fc15651b0eaf28d57fbb
neriaruta <rutaneria2306@gmail.com>
2023-11-04 11:50:34 +0300

    My Django Girls app, first commit

---
+++ example.py
@@ -0,0 +1,8 @@
+# a function that greets
+
+def greet(name):
+    print(name)
+
+while True:
+    name = input("what is your name?: ")
+    greet(name)
---
+++ password.py
@@ -0,0 +1,9 @@
+correct_password ="1234"
+
+user_password =input("enter password" )
+
+while user_password !=correct_password:
+    print("wrong passoword")
+    user_password =input("enter password again:")
+
+print("hello there!")
